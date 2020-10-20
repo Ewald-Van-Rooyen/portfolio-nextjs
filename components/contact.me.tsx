06edaee351813ac64cd5107edcb76d45ce86569a
Ewald Van Rooyen <ewald.vanrooyen@peachpayments.com>
2020-10-20 11:08:27 +0200

    Additions
    README.IO updates
    spelling corrections
    added tootlip css

--- components/contact.me.tsx
+++ components/contact.me.tsx
@@ -4,7 +4,7 @@ import {HiOutlineMail} from "react-icons/hi";
 const ContactMe: React.FunctionComponent = () => {
     return (<>
             <h2 className={"is-size-4 is-family-monospace has-text-white"}>
-                like what you see? Want to get in touch? Buy me a coffee?
+                Like what you see? Want to get in touch? Buy me a coffee?
             </h2>
             <h3 className={"container-min-width mouse-over is-size-4 is-family-monospace has-text-white multi-ani"}>
                 <a className={"has-text-white"} href={"mailto:mailmevanrooyen@gmail.com"}>Contact
--- components/hello.world.tsx
+++ components/hello.world.tsx
@@ -4,18 +4,26 @@ const HelloWorld: React.FunctionComponent = () => {
     return (
         <div className={"columns is-mobile level"}>
             <p className={"is-size-4 is-family-monospace has-text-white is-size-6-mobile"}>
-                I build full stack, half stack, PWA or CMS application...
-                and I use JavaScript to achieve this.
+                I build all the stack applications, full, half, PWA, CMS.
+                You name it and i'll do it.. In Javascript.
 
                 <br/>
-                 <br/>
+                <br/>
                 <div className={"is-not-visible-mobile "}>
-                <span className={"is-size-5 is-family-monospace has-text-white"}> Here are a few frameworks that I have used to create
+                <span className={"is-size-5 is-family-monospace has-text-white"}>Here are a few frameworks that I have used to create
                 applications:</span>
 
                     <ul className={"is-size-6 has-text-left"}>
-                        <li>FE: React, Vue, Angular (Been playing around with Svelte)</li>
-                        <li>BE: Nodejs, Express, Hapi, Nestjs</li>
+                        <li>
+                            <div className={"tooltip"}>FE: React, Vue, Angular (Been playing around with Svelte)
+                                <span className="tooltiptext">Front End</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div className={"tooltip"}>BE: Nodejs, Express, Hapi, Nestjs
+                                <span className="tooltiptext">Back End</span>
+                            </div>
+                        </li>
                         <li>Static Websites: Nextjs, Gatsby</li>
                         <li>PWA: Ionic, Nativescript</li>
                     </ul>
